53b135c4fa96b9622ec5bc62a1c713fca123ada1
reeseyang <zime-yx@163.com>
2019-09-09 11:09:03 +0800

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        sayHello();
+    }
+
+    private static void sayHello() {
         System.out.println("hello world");
     }
 }
